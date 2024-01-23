a0614ced4dc8e71aac0b5eab6cdc0c3ca46623a4
Ramonnn <29675067+Ramonnn@users.noreply.github.com>
2024-01-23 16:30:04 +0100

    added card finding based on collector and series id

--- main.py
+++ main.py
@@ -1,37 +1,46 @@
+import time
 import requests
 import pandas as pd
 
 data = pd.read_table("White.txt", header=None, names=["bulk"])
 
-RGX_PATTERN = r"^(\d+) (.+?) \(([\w\d]+)\) (\d+p?\d*) ?([A-Z])?$"
+RGX_PATTERN = r"^(\d+) (.+?) \(([\w\d]+)\) (\d+[a-z]*) ?([A-Z])?$"
 
 data[["Count", "Card_Name", "Set_Code", "Collector_nr", "Foil"]] = data[
     "bulk"
 ].str.extract(RGX_PATTERN)
 
+json_responses = []
 
-def get_magic_card(card_name):
-    base_url = "https://api.scryfall.com/cards/named"
-    params = {"fuzzy": card_name}
+def get_magic_card(set_code, collector_number):
+    # base_url = "https://api.scryfall.com/cards/named"
+    # params = {"fuzzy": card_name}
 
-    response = requests.get(base_url, params=params, timeout=5)
+    base_url = f"https://api.scryfall.com/cards/{set_code.lower()}/{collector_number}"
+    response = requests.get(base_url, timeout=5)
+
+    print(response.url)
 
     if response.status_code == 200:
-        card_data = response.json()
-        return card_data
+        json_responses.append(response.json())
+        print(f"Card added: {response.json()['name']}")
+    else:
+        print(f"Error: {response.status_code}")
 
-    print(f"Error: {response.status_code}")
-    return None
+for index, row in data.iterrows():
+    get_magic_card(row['Set_Code'],row['Collector_nr'])
+    time.sleep(1)
+
+combined_data = pd.concat(
+    [pd.json_normalize(response) for response in json_responses], ignore_index=True
+)
+
+# if result:
+#    print(f"Name: {result['name']}")
+#    print(f"Type: {result['type_line']}")
+#    print(f"Mana Cost: {result['mana_cost']}")
+# else:
+#    print("Card not found or error in fetching data.")
 
 
-result = get_magic_card(data["Card_Name"][5])
-
-if result:
-    print(f"Name: {result['name']}")
-    print(f"Type: {result['type_line']}")
-    print(f"Mana Cost: {result['mana_cost']}")
-else:
-    print("Card not found or error in fetching data.")
-
-
-print(data.head(10))
+print(combined_data.head(10))
